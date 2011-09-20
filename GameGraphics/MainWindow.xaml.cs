8f34b6e39d018feba3b07cc6a7713c1a0c735ad9
Joe White <git@excastle.com>
2011-09-20 14:46:31 -0500

    Added multi-resolution support.

--- GameGraphics/MainWindow.xaml.cs
+++ GameGraphics/MainWindow.xaml.cs
@@ -16,12 +16,14 @@ namespace GameGraphics
     public partial class MainWindow
     {
         private readonly IList<ControllerViewModel> _allControllerViewModels;
+        private readonly IList<ResolutionViewModel> _allResolutionViewModels;
         private readonly ObservableCollection<RunResults> _runResults = new ObservableCollection<RunResults>();
 
         public MainWindow()
         {
             InitializeComponent();
             Loaded += (sender, e) => ClearValue(SizeToContentProperty);
+
             _allControllerViewModels =
                 (from type in GetType().Assembly.GetTypes()
                  where !type.IsInterface && typeof(IController).IsAssignableFrom(type)
@@ -30,6 +32,18 @@ namespace GameGraphics
                  select viewModel).ToList();
             _allControllerViewModels.First().IsChecked = true;
             ControllerGroups.ItemsSource = _allControllerViewModels.GroupBy(viewModel => viewModel.Library);
+
+            _allResolutionViewModels = new[] {
+                new ResolutionViewModel(800, 600, 50, 42),
+                new ResolutionViewModel(1024, 768, 64, 54),
+                new ResolutionViewModel(1280, 1024, 80, 73),
+                new ResolutionViewModel(1440, 900, 90, 64),
+            };
+            _allResolutionViewModels.Last(
+                res => res.Width <= SystemParameters.PrimaryScreenWidth &&
+                res.Height <= SystemParameters.PrimaryScreenHeight).IsChecked = true;
+            Resolutions.ItemsSource = _allResolutionViewModels;
+
             RunResults.ItemsSource = _runResults;
         }
 
@@ -52,15 +66,17 @@ namespace GameGraphics
         }
         private void Run()
         {
-            var viewModel = _allControllerViewModels.First(vm => vm.IsChecked);
-            var controller = viewModel.Controller;
-            Run(controller);
+            var controllerViewModel = _allControllerViewModels.First(vm => vm.IsChecked);
+            var controller = controllerViewModel.Controller;
+
+            var resolutionViewModel = _allResolutionViewModels.First(vm => vm.IsChecked);
+            Run(controller, resolutionViewModel.DirectoryName, resolutionViewModel.TileWidth, resolutionViewModel.TileHeight);
         }
-        private void Run(IController controller)
+        private void Run(IController controller, string directoryName, int tileWidth, int tileHeight)
         {
             var exeDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-            var imageDirectory = Path.Combine(exeDirectory, "Images", "1440x900");
-            var options = new SceneOptions(imageDirectory, 90, 64);
+            var imageDirectory = Path.Combine(exeDirectory, "Images", directoryName);
+            var options = new SceneOptions(imageDirectory, tileWidth, tileHeight);
             var scene = new Scene(options);
             controller.Run(scene, options);
             LogResults(controller, scene);
---
+++ GameGraphics/ResolutionViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace GameGraphics
+{
+    public class ResolutionViewModel
+    {
+        public ResolutionViewModel(int width, int height, int tileWidth, int tileHeight)
+        {
+            Width = width;
+            Height = height;
+            TileWidth = tileWidth;
+            TileHeight = tileHeight;
+        }
+
+        public string Description
+        {
+            get { return string.Format("{0} x {1}", Width, Height); }
+        }
+        public string DirectoryName
+        {
+            get { return string.Format("{0}x{1}", Width, Height); }
+        }
+        public int Height { get; private set; }
+        public bool IsChecked { get; set; }
+        public int TileHeight { get; private set; }
+        public int TileWidth { get; private set; }
+        public int Width { get; private set; }
+    }
+}
