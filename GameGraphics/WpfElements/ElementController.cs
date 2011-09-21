4a943cd04d7dbfeb69dccba33058ebaf00f4b336
Joe White <git@excastle.com>
2011-09-21 07:49:57 -0500

    Add UI that directly creates and moves WPF elements.

---
+++ GameGraphics/WpfElements/ElementController.cs
@@ -0,0 +1,26 @@
+﻿using GameGraphics.Core;
+using GameGraphics.WpfCommon;
+
+namespace GameGraphics.WpfElements
+{
+    public class ElementController : IController
+    {
+        public string Description
+        {
+            get { return "Elements"; }
+        }
+        public Library Library
+        {
+            get { return Library.Wpf; }
+        }
+
+        public void Run(Scene scene, SceneOptions options)
+        {
+            var window = new GraphicsWindow(scene);
+            var view = new ElementView(options);
+            scene.Initialize(view);
+            window.Content = view;
+            window.ShowDialog();
+        }
+    }
+}
---
+++ GameGraphics/WpfElements/ElementSprite.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Controls;
+using GameGraphics.Core;
+
+namespace GameGraphics.WpfElements
+{
+    public class ElementSprite : ISprite
+    {
+        private readonly Image _imageElement;
+        private readonly SceneOptions _sceneOptions;
+
+        public ElementSprite(Image imageElement, SceneOptions sceneOptions)
+        {
+            _imageElement = imageElement;
+            _sceneOptions = sceneOptions;
+        }
+
+        public double X
+        {
+            get { return Canvas.GetLeft(_imageElement) / _sceneOptions.TileWidth; }
+            set { Canvas.SetLeft(_imageElement, value * _sceneOptions.TileWidth); }
+        }
+
+        public double Y
+        {
+            get { return Canvas.GetTop(_imageElement) / _sceneOptions.TileHeight; }
+            set { Canvas.SetTop(_imageElement, value * _sceneOptions.TileHeight); }
+        }
+    }
+}
